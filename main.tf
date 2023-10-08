fe56ee04f2ab6a1c7064d65e9ce3a1ddaa944e81
SaiKasaragadda1 <119982694+SaiKasaragadda1@users.noreply.github.com>
2023-10-08 17:32:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,35 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami="ami-0aec300fa613b1c92"
   instance_type= "t2.micro"
+  security_groups=["aws_security_group.sai.name"]
   tags={
       Name= "Sai-Server1"
+  }
+}
+
+resource "aws_security_group" "sai_securitygroup" {
+  name        = "sai_securitygroup"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = vpc-0beed584dea86fdbe
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sai_securitygroup"
   }
 }
