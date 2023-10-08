e067c994e7151db8186c91afcbc486755ecb3178
SaiKasaragadda1 <119982694+SaiKasaragadda1@users.noreply.github.com>
2023-10-07 20:43:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region= "us-east-2"
 }
-resource "aws-instance" "example" {
+resource "aws_instance" "example" {
   ami="ami-0aec300fa613b1c92"
   instance_type= "t2.micro"
   tags={
